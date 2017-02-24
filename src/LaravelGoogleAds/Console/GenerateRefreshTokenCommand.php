12b8e952e59daf9191dcd0be30c096b0a6dce718
Nikolaj Petersen <nikolaj.lovenhardt@gmail.com>
2017-02-24 12:54:41 +0100

    Refresh token command

--- src/LaravelGoogleAds/Console/GenerateRefreshTokenCommand.php
+++ src/LaravelGoogleAds/Console/GenerateRefreshTokenCommand.php
@@ -3,12 +3,31 @@
 namespace LaravelGoogleAds\Console;
 
 use Exception;
-use OAuth2Exception;
+use Google\Auth\OAuth2;
 use Illuminate\Console\Command;
+use Google\Auth\CredentialsLoader;
 use LaravelGoogleAds\Services\AdWordsService;
 
 class GenerateRefreshTokenCommand extends Command
 {
+    /**
+     * @var string the OAuth2 scope for the AdWords API
+     * @see https://developers.google.com/adwords/api/docs/guides/authentication#scope
+     */
+    const ADWORDS_API_SCOPE = 'https://www.googleapis.com/auth/adwords';
+
+    /**
+     * @var string the Google OAuth2 authorization URI for OAuth2 requests
+     * @see https://developers.google.com/identity/protocols/OAuth2InstalledApp#formingtheurl
+     */
+    const AUTHORIZATION_URI = 'https://accounts.google.com/o/oauth2/v2/auth';
+
+    /**
+     * @var string the redirect URI for OAuth2 installed application flows
+     * @see https://developers.google.com/identity/protocols/OAuth2InstalledApp#formingtheurl
+     */
+    const REDIRECT_URI = 'urn:ietf:wg:oauth:2.0:oob';
+
     /**
      * Console command signature
      *
@@ -23,34 +42,44 @@ class GenerateRefreshTokenCommand extends Command
      */
     protected $description = 'Generate a new refresh token for Google Ads API';
 
-    /**
-     * Generate refresh token
-     *
-     * @param AdWordsService $adWordsService
-     */
-    public function fire(AdWordsService $adWordsService)
+    public function fire()
     {
-        $authorizationUrl = $adWordsService->getOAuthAuthorizationUrl($user);
+        if (!$config = $this->config()) {
+            return $this->error('Please provide a valid configuration for Laravel Google Ads');
+        }
 
-        // Post authorization URL
+        $clientId = $config['clientId'];
+        $clientSecret = $config['clientSecret'];
+        $scopes = self::ADWORDS_API_SCOPE;
+
+        $oauth2 = new OAuth2([
+            'authorizationUri' => self::AUTHORIZATION_URI,
+            'redirectUri' => self::REDIRECT_URI,
+            'tokenCredentialUri' => CredentialsLoader::TOKEN_CREDENTIAL_URI,
+            'clientId' => $clientId,
+            'clientSecret' => $clientSecret,
+            'scope' => $scopes
+        ]);
+
         $this->line(sprintf(
             "Please sign in to your AdWords account, and open following url:\n%s",
-            $authorizationUrl
+            $oauth2->buildFullAuthorizationUri([
+                'access_type' => 'offline',
+            ])
         ));
 
         // Retrieve token
-        $accessToken = $this->ask('Insert your access token:');
+        $accessToken = $this->ask('Insert your access token');
 
+        // Fetch auth token
         try {
-            $oAuth2Info = $adWordsService->getOAuthCredentials($user, $accessToken);
-        } catch (OAuth2Exception $e) {
-            // OAuth2 error
-            return $this->error('[OAUTH2]: ' . $e->getMessage());
-        } catch (Exception $e) {
-            return $this->error($e->getMessage());
+            $oauth2->setCode($accessToken);
+            $authToken = $oauth2->fetchAuthToken();
+        } catch (Exception $exception) {
+            return $this->error($exception->getMessage());
         }
 
-        if (!isset($oAuth2Info['refresh_token'])) {
+        if (!isset($authToken)) {
             return $this->error('Error fetching the refresh token');
         }
 
@@ -59,7 +88,24 @@ class GenerateRefreshTokenCommand extends Command
         // Print refresh token
         $this->line(sprintf(
             'Refresh token: "%s"',
-            $oAuth2Info['refresh_token']
+            $authToken['refresh_token']
         ));
+    }
+
+    /**
+     * Configuration
+     *
+     * @return bool|array
+     */
+    private function config()
+    {
+        /** @var null|array $config */
+        $config = config('google-ads');
+
+        if (is_null($config) || !count($config)) {
+            return false;
+        }
+
+        return $config['OAUTH2'];
     }
 }
