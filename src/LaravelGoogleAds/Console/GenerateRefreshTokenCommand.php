484a47d20d5a848e3db4a53acce5cef720d1f61e
Nikolaj Løvenhardt Petersen <nikolaj.lovenhardt@gmail.com>
2017-07-03 12:26:02 +0200

    Refactoring

--- src/LaravelGoogleAds/Console/GenerateRefreshTokenCommand.php
+++ src/LaravelGoogleAds/Console/GenerateRefreshTokenCommand.php
@@ -3,80 +3,62 @@
 namespace LaravelGoogleAds\Console;
 
 use Exception;
-use Google\Auth\OAuth2;
 use Illuminate\Console\Command;
-use Google\Auth\CredentialsLoader;
+use LaravelGoogleAds\Services\AuthorizationService;
 
 class GenerateRefreshTokenCommand extends Command
 {
-    /**
-     * @var string the OAuth2 scope for the AdWords API
-     * @see https://developers.google.com/adwords/api/docs/guides/authentication#scope
-     */
-    const ADWORDS_API_SCOPE = 'https://www.googleapis.com/auth/adwords';
-
-    /**
-     * @var string the Google OAuth2 authorization URI for OAuth2 requests
-     * @see https://developers.google.com/identity/protocols/OAuth2InstalledApp#formingtheurl
-     */
-    const AUTHORIZATION_URI = 'https://accounts.google.com/o/oauth2/v2/auth';
-
-    /**
-     * @var string the redirect URI for OAuth2 installed application flows
-     * @see https://developers.google.com/identity/protocols/OAuth2InstalledApp#formingtheurl
-     */
-    const REDIRECT_URI = 'urn:ietf:wg:oauth:2.0:oob';
-
-    /**
-     * Console command signature
-     *
-     * @var string
-     */
+    /** @var string */
     protected $signature = 'googleads:token:generate';
 
-    /**
-     * Description
-     *
-     * @var string
-     */
+    /** @var string */
     protected $description = 'Generate a new refresh token for Google Ads API';
+
+    /** @var AuthorizationService */
+    protected $authorizationService;
+
+    /**
+     * GenerateRefreshTokenCommand constructor.
+     * @param AuthorizationService $authorizationService
+     */
+    public function __construct(AuthorizationService $authorizationService)
+    {
+        parent::__construct();
+
+        $this->authorizationService = $authorizationService;
+    }
 
     /**
      * Generate command
      */
     public function fire()
     {
+        $authorizationService = $this->authorizationService;
+
         if (!$config = $this->config()) {
-            return $this->error('Please provide a valid configuration for Laravel Google Ads');
+            $this->error('Please provide a valid configuration for Laravel Google Ads');
+            return;
         }
 
         $clientId = $config['clientId'];
         $clientSecret = $config['clientSecret'];
-        $scopes = self::ADWORDS_API_SCOPE;
 
-        $oauth2 = new OAuth2([
-            'authorizationUri' => self::AUTHORIZATION_URI,
-            'redirectUri' => self::REDIRECT_URI,
-            'tokenCredentialUri' => CredentialsLoader::TOKEN_CREDENTIAL_URI,
-            'clientId' => $clientId,
-            'clientSecret' => $clientSecret,
-            'scope' => $scopes
-        ]);
+        $oauth2 = $authorizationService->oauth2($clientId, $clientSecret, 'http://slapi.dev/test2');
 
         $this->line(sprintf(
             "Please sign in to your AdWords account, and open following url:\n%s",
-            $oauth2->buildFullAuthorizationUri([
-                'access_type' => 'offline',
+            $authorizationService->buildFullAuthorizationUri($oauth2, true, [
+                'prompt' => 'consent',
             ])
         ));
 
         // Retrieve token
-        $accessToken = $this->ask('Insert your access token');
+        $accessToken = $this->ask('Insert the code you received from Google');
 
         // Fetch auth token
+
         try {
-            $oauth2->setCode($accessToken);
-            $authToken = $oauth2->fetchAuthToken();
+            $authToken = $authorizationService->fetchAuthToken($oauth2, $accessToken);
         } catch (Exception $exception) {
             return $this->error($exception->getMessage());
         }
